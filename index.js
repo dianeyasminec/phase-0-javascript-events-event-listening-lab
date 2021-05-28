c474ad78358672f46531d70f261a05ccc37575d5
dianeyasminec <dianeyasminec60@gmail.com>
2021-05-28 15:17:17 -0400

    completed assignement

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
-function addingEventListener() {
+
+function clickAlert() {
+  alert('I was clicked!');
 }
+function addingEventListener() {
+  const input = document.getElementById('input');
+  input.addEventListener('click', clickAlert); 
+}
+
+
+
